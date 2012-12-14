5b91ee8a077c3efa99218460153eca4d6a5e2f7e
mahamadou traore <benybei@hotmail.fr>
2012-12-14 11:31:00 -0500

    sa marche amene la liste des equipe bien regarde le json pour mettre le bon lien

--- app/view/Espagne.js
+++ app/view/Espagne.js
@@ -20,24 +20,20 @@ Ext.define('sport.view.Espagne',{
         items: [
                 {
                 	xtype:'list',
-                	itemTpl:'{name}',
+                	itemTpl:'{location}',
                 	title:'Recent Post',
                 	 store: {                       
                 		 autoLoad:true,
 
-                         fields: ['name'],
+                         fields: ['location'],
                         
                          proxy: {
                              type: 'jsonp',
                              url: 'http://api.espn.com/v1/sports/soccer/fra.1/teams/news/?apikey=jcsjfv8gj7mf34hnm6qzxt72',
                              reader: {
                             	 type: 'json',
-                            	  // record: 'leagues',
-                            	    //rootProperty: 'users'
-                            	 
-                            	// rootProperty:Ext.encode('sports')+'.'+'leagues',
-                                //callbackKey: 'callback',
-                            	 //rootProperty:'sports[0].leagues',
+                            	 /* sa marche amene la liste des equipe*/
+                            	 rootProperty:'sports[0].leagues[0].teams',
                              }
                          }
                      }
